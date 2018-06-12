9219c831458c288bfa3304a7ae7738a67a34c678
David Stolarsky <david.stolarsky@nytimes.com>
2018-06-12 14:36:00 -0400

    Initial Commit

---
+++ gltf2scn/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gltf2scn
+//
+//  Created by Stolarsky, David on 6/12/18.
+//  Copyright © 2018 The New York Times. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
