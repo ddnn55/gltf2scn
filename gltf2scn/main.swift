2b5a5e45651490f4fee3a941e4f7e19c48673ead
David Stolarsky <david.stolarsky@nytimes.com>
2018-10-09 12:50:56 -0400

    existing functionality working with Commander

--- gltf2scn/main.swift
+++ gltf2scn/main.swift
@@ -10,7 +10,12 @@ import Foundation
 import SceneKit
 
 
-
+//class ExportDelegate: NSObject, SCNSceneExportDelegate {
+//    func write(_ image: NSImage, withSceneDocumentURL documentURL: URL, originalImageURL: URL?) -> URL? {
+//        print("hello from image export func!")
+//        return URL(string: "https://placekitten.com/g/1024/1024")
+//    }
+//}
 
 
 command(
@@ -19,48 +24,29 @@ command(
     Argument<String>("outputPath", description: ".scn output path")
 ) { merge, path, outputPath in
     print("input \(path) output: \(outputPath) merge: \(merge)")
+    
+//    let exportDelegate = ExportDelegate()
+    
+    var scene: SCNScene
+    do {
+        let sceneSource = try GLTFSceneSource(path: path)
+        scene = try sceneSource.scene()
+        let success = scene.write(to: URL(fileURLWithPath: outputPath), options: nil, delegate: nil, progressHandler: nil)
+        if(success) {
+            print("Saved \(outputPath)")
+        }
+        else {
+            print("Error while trying to save \(outputPath)")
+            exit(1)
+        }
+    } catch {
+        print("Error while converting scene: \(error.localizedDescription)")
+        exit(1)
+    }
+    
 }.run()
 
 
 
 
 
-
-
-if(CommandLine.arguments.count < 2) {
-    print("Usage: gltf2scn <path to .glb> [.scn output path]")
-    exit(0)
-}
-
-let path = CommandLine.arguments[1]
-let sceneName = URL(fileURLWithPath: path).deletingPathExtension().lastPathComponent
-
-let outputPath = CommandLine.arguments.count < 3 ? "\(sceneName).scn" : CommandLine.arguments[2]
-
-print("creating exportDelegate")
-
-class ExportDelegate: NSObject, SCNSceneExportDelegate {
-    func write(_ image: NSImage, withSceneDocumentURL documentURL: URL, originalImageURL: URL?) -> URL? {
-        print("hello from image export func!")
-        return URL(string: "https://placekitten.com/g/1024/1024")
-    }
-}
-
-let exportDelegate = ExportDelegate()
-
-var scene: SCNScene
-do {
-    let sceneSource = try GLTFSceneSource(path: path)
-    scene = try sceneSource.scene()
-    let success = scene.write(to: URL(fileURLWithPath: outputPath), options: nil, delegate: exportDelegate, progressHandler: nil)
-    if(success) {
-        print("Saved \(outputPath)")
-    }
-    else {
-        print("Error while trying to save \(outputPath)")
-        exit(1)
-    }
-} catch {
-    print("Error while converting scene: \(error.localizedDescription)")
-    exit(1)
-}
